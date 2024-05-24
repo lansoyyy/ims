7404cbe959c59c380ac563c6227baaf71a5c9028
Lans12345 <olanalans12345@gmail.com>
2024-05-24 13:13:24 +0800

    project structure

--- lib/utlis/colors.dart
+++ lib/utlis/colors.dart
@@ -2,8 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-var primary = const Color(0xff1A1851);
+var primary = const Color(0xffE82B39);
 
-var secondary = const Color(0xffFCB315);
+var secondary = const Color(0xffDFDFDF);
 var black = const Color(0xff151515);
 var grey = const Color(0xffB3B3B3);
