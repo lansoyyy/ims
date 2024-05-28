701201dd036007578fde59b7446c07eb622acf75
Lans12345 <olanalans12345@gmail.com>
2024-05-28 10:12:23 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ims/firebase_options.dart';
 import 'package:ims/screens/home_screen.dart';
 import 'package:ims/screens/login_screen.dart';
 import 'package:ims/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'information-management-s-9f909',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
